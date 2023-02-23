fcd36c010528cc6fe45f07ddc95ae12bfeedafe4
Hamza <anwarhamza919@gmail.com>
2023-02-23 14:51:40 -0800

    react firebase google auth

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import {auth, provider} from './FirebaseConfig';
+import {useState} from 'react';
+import { signInWithPopup } from 'firebase/auth';
 
 function App() {
+
+  const [user, setUser] = useState(null);
+
+  const handleGoogleSignIn=()=>{
+    signInWithPopup(auth, provider).then((result)=>{
+      const user = result.user;
+      console.log(user);
+      setUser(user);
+    }).catch((err)=>{
+      console.log(err);
+    })
+  }
+
+  const handleLogout=()=>{
+    setUser(null);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <div className='box'>
+          {user?(
+            <>
+              <button className='btn btn-secondary btn-md'
+                onClick={handleLogout}>
+                LOGOUT
+              </button>
+              <h3>Welcome {user.displayName}</h3>
+              <p>{user.email}</p>
+              <div className='photo'>
+                <img src={user.photoURL} alt="dp" referrerPolicy='no-referrer'/>
+              </div>
+            </>
+          ):(
+            <button className='btn btn-danger btn-md'
+              onClick={handleGoogleSignIn}>
+              Sign In With Google
+            </button>  
+          )} 
+      </div>
     </div>
   );
 }
---
+++ src/FirebaseConfig.js
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCwL4mYYGeuqjkYgktTr3fEp4MlUc7GTm4",
+  authDomain: "hamza-portfolio-f30ba.firebaseapp.com",
+  projectId: "hamza-portfolio-f30ba",
+  storageBucket: "hamza-portfolio-f30ba.appspot.com",
+  messagingSenderId: "526692752996",
+  appId: "1:526692752996:web:f17741db0f840ad5ee4971",
+  measurementId: "G-TWBJEEF714"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+const provider = new GoogleAuthProvider();
+
+export {auth, provider}
